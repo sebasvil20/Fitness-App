cda706cf309e79db8c0553186ebad1b1789e1d5a
Hinval <sebasvil20@gmail.com>
2020-01-27 09:42:22 -0500

    innitial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+const user={
+    firstName: 'Sebastian',
+    lastName: 'Villegas',
+    avatar: 'https://cdn4.iconfinder.com/data/icons/famous-characters-add-on-vol-1-flat/48/Famous_Character_-_Add_On_1-22-512.png'
+}
+
+function getName(user){
+    return `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user){
+    if(user){
+        return <h1>Hello {getName(user)} </h1>
+    }
+    return <h1>Hello Stranger </h1>
+}
+
+
+const element = (
+    <div>
+        <h1>{getGreeting(user)}</h1>
+        <img src={user.avatar} />
+    </div>
+)
+const container = document.getElementById('root')
+
+ReactDOM.render(element,container);
